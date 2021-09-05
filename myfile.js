ef82ae5fa0206c3a0b3e5f18b58a038770850a94
Michael O <michael7943@gmail.com>
2021-09-05 20:57:21 +1000

    inner examples for text and html

--- myfile.js
+++ myfile.js
@@ -1,3 +1,8 @@
+
+var answerText = document.getElementById("answerTextId");
+var buttonAnswer = document.getElementById("showAnswerId");
+var message = document.querySelector('#showMessage');
+
 function onClickEvent(){
     let mycount = 2
     alert("you have clicked"+mycount+"exercise")
@@ -9,3 +14,10 @@ function nestedcall(count){
     console.log("reached nestedcall")
     return count+6
 }
+
+function showAnswer() {
+	answerText.innerText = "No. Hyperlinks can be used on both text and images.";
+    message.innerHTML = "<span style='color:red;'>You have revealed the answer.</span>";
+}
+
+buttonAnswer.addEventListener("click", showAnswer);
